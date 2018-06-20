6976863f248d8f7d2f96a433cb6da6d7ba8fb21b
vagrant <vagrant@htanaka0828.devbase.test>
2018-06-20 22:56:35 +0000

    ssh鍵の登録

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ sudo apt-get dist-upgrade
 
 sudo curl -fsSL https://get.docker.com/ | sh
 sudo usermod -aG docker vagrant
-curl -L https://github.com/docker/compose/releases/download/1.10.0/docker-compose-`uname -s`-`uname -m` > docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` > docker-compose
 sudo mv docker-compose /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo service docker start
@@ -22,3 +22,8 @@ sudo apt-get -y install samba
 sudo cp -p /vagrant/conf/smb.conf /etc/samba/smb.conf
 sudo chown root:root /etc/samba/smb.conf
 sudo /etc/init.d/samba restart
+
+if [ ! -d ~/.ssh ]; then
+  cp -p ssh/id_rsa ~/.ssh
+  chmod 600 ~/.ssh/id_rsa
+fi
